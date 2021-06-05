6b7af8494a5e16080aedee693f5eb82b622bc721
Adam Jonas <adamjonas25@gmail.com>
2021-06-05 17:26:52 +0100

    Add go button

--- test/integration/enteringAUser.spec.js
+++ test/integration/enteringAUser.spec.js
@@ -1,4 +1,4 @@
-describe('Index page', () => {
+describe('Entering a user', () => {
   it('lets you enter a github username', () => {
     cy.visit('/')
 
@@ -6,5 +6,13 @@ describe('Index page', () => {
       .type('aj8gh')
 
     cy.get('#username').should('have.value', 'aj8gh')
+  })
+
+  it('lets you click go', () => {
+    cy.visit('/')
+
+    cy.get('#username').type('aj8gh')
+
+    cy.get('#go').click()
   })
 })
